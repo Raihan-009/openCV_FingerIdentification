6f8fbcfeeb8fba494455058d19fa071ac9758a4b
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-04-14 00:28:02 +0600

    finger identification project done

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import cv2
 import handTracker as ht
 import fingersOrientation as fo
+import fingerIdentification as fi
 
+cam = cv2.VideoCapture(0)
+handTracker = ht.handTracker()
+fingerTracker = fo.FingerOrientation()
+fingerIdentify = fi.FingerIdentification()
+while True:
+    ret, frame = cam.read()
+    if ret:
+        hands = handTracker.findHands(frame)
+        if hands:
+            oneHand = hands[0]
+            finger_1 = fingerTracker.fingerOrientation(oneHand)
+            fingerIdentify.fingerIdentification(finger_1)
+
+        cv2.imshow("Framing", frame)
+    else:
+        break
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()

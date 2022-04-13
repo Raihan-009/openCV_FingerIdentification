1a3dee5fdd495770e979a87d4074214320cbf708
Light Yagami <lightyagami@Lights-MacBook-Air.local>
2022-04-13 15:33:04 +0600

    venv added

---
+++ main.py
@@ -0,0 +1,71 @@
+import cv2
+import mediapipe as mp
+
+
+class handDetector():
+    def __init__(self, 
+                mode=False, 
+                maxHands = 2, 
+                modelComplexity=1, 
+                detectionCon = 0.5, 
+                trackCon = 0.5):
+                
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComplexity = modelComplexity
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+        self.tipIds = [4,8,12,16,20]
+    
+    def findHands(self, img, drawL = True, drawB = True, mirror = True):
+        rgbImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(rgbImg)
+        allHands = []
+        h,w,c = img.shape
+
+        if self.results.multi_hand_landmarks:
+            for handType, handLms in zip(self.results.multi_handedness,self.results.multi_hand_landmarks):
+                myHand = {}
+                lmList = []
+                xList = []
+                yList = [] 
+                for id, lm in enumerate(handLms.landmark):
+                    cx, cy = int(lm.x*w), int(lm.y*h)
+                    lmList.append([id, cx, cy])
+                    xList.append(cx)
+                    yList.append(cy)
+
+                #bounding box
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+
+                box_W = xmax - xmin
+                box_H = ymax - ymin
+
+                bbox = xmin, ymin, box_W, box_H
+                myHand["landmarksList"] = lmList
+                myHand["bbox"] = bbox
+                
+                if mirror:
+                    if handType.classification[0].label == "Right":
+                        myHand["hand_type"] = "Left Hand"
+                    else:
+                        myHand["hand_type"] = "Right Hand"
+                else:
+                    myHand["hand_type"] = handType.classification[0].label
+                allHands.append(myHand)
+
+                #draw utils
+                if drawL:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+                if drawB:
+                    cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+                                  (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+                                  (255, 0, 255), 2)
+        return allHands
